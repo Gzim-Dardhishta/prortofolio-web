fe558cdd3fbe5c644802d63d646e4732113ab44c
Gzim-DArdhishta <gzimdardhishta0gmail.com>
2021-12-06 15:02:26 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("Hello")
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".links");
+const ul_links = document.querySelectorAll(".links li");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("open");
+});
