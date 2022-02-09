0793aeb2943d0cdd7e58020f1aaab06e4440b992
Gzim-DArdhishta <gzimdardhishta0gmail.com>
2022-02-09 02:21:19 +0100

    Added Slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+// close the hamburger menu
+const navLink = document.querySelectorAll(".nav-menu");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
 
 
--- js/validation.js
+++ js/validation.js
@@ -1,61 +1,113 @@
-// message error
-var FirstNameMsg = document.getElementById("nameMsg");
+var button = document.getElementById("send");
+
+var usernameMsg = document.getElementById("nameMsg");
 var emailMsg = document.getElementById("emailMsg");
 var messageMsg = document.getElementById("messageMsg");
 
-var button = document.querySelector("send");
+var usernameRegex = /^[A-Z]\w+[._-]?\w+/;
+var emailRegex = /^[A-Za-z]\w+[._-@]\w+[0-9]\d/;
 
 
 button.addEventListener("click", function(event){
-
-    // getting name, email, message values from the form
-    var name = document.getElementById("name").value;
+    var username = document.getElementById("name").value;
     var email = document.getElementById("e-mail").value;
     var message = document.getElementById("message").value;
 
-    var usernameRegex = /^[A-Za-z]\w/;
-    var emailRegex = /^[A-Za-z]\w+[._-@]\w+[0-9]\d/;
-    var messageRegex = /^[A-Za-z]\w/;
 
-    // checking the FirstName field
-    if(name == "" || name == null){
-        usernameMsg.innerText="Fill Firstname field!";
+    if(username == "" || username == null){
+        usernameMsg.innerText="Fill Username field!";
         event.preventDefault();
-        return false;
     }else{
-        if(usernameRegex.test(name)){
+        if(usernameRegex.test(username)){
             usernameMsg.innerText="";
         }else{
             usernameMsg.innerText="Username Error!";
-            // event.preventDefault();
-            return false;
+            event.preventDefault();
+        }
+
+    }
+
+    if (email == "" || email == null){
+        emailMsg.innerText = "Fill the E-mail field!"
+        event.preventDefault();
+    }
+    else {
+        if(emailRegex.test(email)){
+            emailMsg.innerText = "";
+        }
+        else{
+            emailMsg.innerText = "Email Error!!";
+            event.preventDefault();
         }
     }
 
-    // // checking the E-mail field
-    // if(email == "" || email == null){
-    //     emailMsg.innerText="Fill E-mail field!";
-    //     event.preventDefault();
-    // }else{
-    //     if(usernameRegex.test(email)){
-    //         emailMsg.innerText="";
-    //     }else{
-    //         emailMsg.innerText="E-mail Error!";
-    //         event.preventDefault();
-    //     }
-    // }
-
-    // // checking the Message field
-    // if(message == "" || message == null){
-    //     messageMsg.innerText="Fill Message field!";
-    //     event.preventDefault();
-    // }else{
-    //     if(usernameRegex.test(email)){
-    //         messageMsg.innerText="";
-    //     }else{
-    //         messageMsg.innerText="E-mail Error!";
-    //         event.preventDefault();
-    //     }
-    // }
-
+    // checking the Message field
+    if(message == "" || message == null){
+        messageMsg.innerHTML="Fill Message field!";
+        event.preventDefault();
+    }
 })
+
+
+
+
+
+// // message error
+// var FirstNameMsg = document.getElementById("nameMsg");
+// var emailMsg = document.getElementById("emailMsg");
+// var messageMsg = document.getElementById("messageMsg");
+
+// var button = document.getElementById("send");
+
+
+// button.addEventListener("click", function(event){
+
+//     // getting name, email, message values from the form
+//     var name = document.getElementById("name").value;
+//     var email = document.getElementById("e-mail").value;
+//     var message = document.getElementById("message").value;
+
+//     var usernameRegex = /^[A-Z]\w+[._-]?\w+/;
+//     var emailRegex = /^[A-Za-z]\w+[._-@]\w+[0-9]\d/;
+//     var messageRegex = /^[A-Z]\w+[._-]?\w+/;
+
+//     // checking the FirstName field
+//     if(name == "" || name == null){
+//         usernameMsg.innerHTML="Fill Firstname field!";
+//         event.preventDefault();
+//     }else{
+//         if(usernameRegex.test(name)){
+//             usernameMsg.innerHTML="";
+//         }else{
+//             usernameMsg.innerHTML="Username Error!";
+//             event.preventDefault();
+//         }
+//     }
+
+//     // checking the E-mail field
+//     if(email == "" || email == null){
+//         emailMsg.innerHTML="Fill E-mail field!";
+//         event.preventDefault();
+//     }else{
+//         if(emailRegex.test(email)){
+//             emailMsg.innerHTML="";
+//         }else{
+//             emailMsg.innerHTML="E-mail Error!";
+//             event.preventDefault();
+//         }
+//     }
+
+//     // checking the Message field
+//     if(message == "" || message == null){
+//         messageMsg.innerHTML="Fill Message field!";
+//         event.preventDefault();
+//     }else{
+//         if(messageRegex.test(email)){
+//             messageMsg.innerHTML="";
+//         }else{
+//             messageMsg.innerHTML="E-mail Error!";
+//             event.preventDefault();
+//         }
+//     }
+
+// })
